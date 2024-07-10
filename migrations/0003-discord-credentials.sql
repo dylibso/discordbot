29f16d5d1fe6e68cf844142e37e3c9929157d930
Muhammad Azeez <muhammad@dylibso.com>
2024-07-10 11:56:50 +0300

    seprate migration into two stages

--- migrations/0003-discord-credentials.sql
+++ migrations/0003-discord-credentials.sql
@@ -1,2 +1,1 @@
 alter type credential_type add value 'discord';
-create index "credentials_discord_idx" on "credentials" using gin("data") where "deleted_at" is null and "type" = 'discord' and "data" ? 'id';
