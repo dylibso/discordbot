b96cb744b44bf4280ea203c1a72061b86b4df7e6
Chris Dickinson <chris@dylibso.com>
2024-10-03 11:47:14 -0700

    remove older examples

--- examples/LOUDBOT/esbuild.js
+++
@@ -1,9 +0,0 @@
-const esbuild = require('esbuild');
-
-esbuild.build({
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  outfile: 'dist/index.js',
-  target: 'es2020',
-  format: 'cjs',
-}).catch(() => process.exit(1));
